a7f101ec3d353df33e1a9296335cc2fd0c39bee7
HieuPhamBa <45411070+HieuPhamBa@users.noreply.github.com>
2020-11-09 00:56:27 +0700

    complete lesson 13

--- main.js
+++ main.js
@@ -42,6 +42,16 @@ window.onload = () => {
     const ulPhone = document.getElementById('ul-phone')
     const ulTV = document.getElementById('ul-tv')
 
+    const hideProductUls = (uls) => {
+      uls.forEach(ul => {
+        ul.style.display = 'none'
+      });
+    }
+
+    const showProductUl = (ul) => {
+      ul.style.display = 'block'
+    }
+
     hideProductUls([ulLaptop, ulPhone, ulTV])
 
     switch (productTyle) {
@@ -57,16 +67,138 @@ window.onload = () => {
     }
   })
 
-  function hideProductUls(uls) {
-    uls.forEach(ul => {
-      ul.style.display = 'none'
-    });
-  }
-
-  function showProductUl(ul) {
-    ul.style.display = 'block'
-  }
-
   const event = new Event('change')
   selProductTyle.dispatchEvent(event);
+
+  /*
+    Bai 4
+  */
+  const menu = [
+    {
+      name: 'Home',
+      children: [
+        {
+          name: 'Home 1',
+          children: [
+            {
+              name: 'Home 1.1',
+              children: [
+                { name: 'Home 1.1.1' }
+              ]
+            },
+            {
+              name: 'Home 1.2',
+              children: [
+                { name: 'Home 1.2.1' }
+              ]
+            }
+          ]
+        },
+        {
+          name: 'Home 2',
+          children: [
+            {
+              name: 'Home 2.1'
+            }
+          ]
+        },
+        {
+          name: 'Home 3',
+          children: [
+            {
+              name: 'Home 3.1'
+            }
+          ]
+        }
+      ]
+    },
+    {
+      name: 'About',
+      children: [
+        {
+          name: 'About 1',
+          children: [
+            {
+              name: 'About 1.1',
+              children: [
+                { name: 'About 1.1.1' }
+              ]
+            },
+            {
+              name: 'About 1.2',
+              children: [
+                {
+                  name: 'About 1.2.1',
+                  children: [
+                    { name: 'About 1.2.1.1' },
+                    { name: 'About 1.2.1.2' },
+                    { name: 'About 1.2.1.3' }
+                  ]
+                }
+              ]
+            }
+          ]
+        },
+        {
+          name: 'Home 2',
+          children: [
+            {
+              name: 'Home 2.1'
+            }
+          ]
+        }
+      ]
+    }
+  ]
+
+  const ulMenu = document.getElementById('menu')
+
+
+  const menuBar = (menus = [], parentElemt = ulMenu, levelMenu = 0) => {
+    const appendLi = (item, parrent, levelMenu) => {
+      const li = document.createElement("li")
+      const a = document.createElement("a")
+
+      a.textContent = item.name
+      li.appendChild(a)
+
+      if (item.children) {
+        appendUl(item, li, levelMenu)
+      }
+
+
+      parrent.appendChild(li)
+    }
+
+    const appendUl = (item, li, levelMenu) => {
+      const ul = document.createElement("ul")
+
+      ul.className = 'child'
+      li.className = 'parent'
+
+      if (levelMenu > 0) {
+        appendSpan(li)
+      }
+
+      menuBar(item.children, ul, levelMenu + 1)
+
+      li.appendChild(ul)
+    }
+
+    const appendSpan = (li) => {
+      const span = document.createElement("span")
+
+      span.className = 'expand'
+      span.textContent = '>>'
+
+      li.appendChild(span)
+    }
+
+    menus.forEach(item => {
+      appendLi(item, parentElemt, levelMenu)
+    })
+  }
+
+  menuBar(menu, ulMenu)
 }
+
