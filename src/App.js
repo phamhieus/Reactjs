c3dd58b419450871474232f80bda764c8920bfe8
phamhieus <45411070+phamhieus@users.noreply.github.com>
2021-05-08 15:14:15 +0700

    learn about basic react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
+import { createStore } from 'redux';
+
+const initState = {
+  value: 0
+}
+
+function counterReducer(state=initState, action) {
+  switch (action.type) {
+    case "counter/incremented":
+      state.value = state.value + 1;
+      break;
+    case "counter/decremented":
+      state.value = state.value - 1;
+      break;
+  }
+
+  return state;
+}
 
 function App() {
+  const [state, setState] = useState(initState);
+
+  const store = createStore(counterReducer);
+
+  const render = () => {
+    const data = store.getState()
+    console.log({data})
+    state.value = data.value
+  }
+
+  store.subscribe(render);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>
+        Clicked: <span id="value">{state.value}</span> times
+        <button id="increment" onClick={e=> store.dispatch({ type: "counter/incremented" })}>+</button>
+        <button id="decrement" onClick={e=> store.dispatch({ type: "counter/decremented" })}>-</button>
+        <button id="incrementIfOdd">Increment if odd</button>
+        <button id="incrementAsync">Increment async</button>
+      </p>
     </div>
   );
 }
